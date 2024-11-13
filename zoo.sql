034336b641f366604e1fd17e802241987f4b1a6e
Tin Pintarić <tin.pinta@gmail.com>
2024-11-13 17:59:21 +0100

    rad na bazi zoo

--- zoo.sql
+++ zoo.sql
@@ -1,8 +1,18 @@
+use master;
+go
+
+drop database if exsist zoo;
+go
+create database zoo;
+ use zoo;
+
 create table zivotinja(
-sifra int,
+sifra int not null primary key identity(1,1),
 vrsta varchar(50),
 ime varchar(50),
 djelatnik int,
 prostorija int,
 datum int,
 );
+
+

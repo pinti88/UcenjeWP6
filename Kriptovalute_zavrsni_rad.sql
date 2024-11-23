3e2e8cc0e151a813ad37eae9b32d27bdfc395533
Gudelj22 <tin.pinta@gmail.com>
2024-11-23 10:21:42 +0100

    Update Kriptovalute_zavrsni_rad.sql

--- Kriptovalute_zavrsni_rad.sql
+++ Kriptovalute_zavrsni_rad.sql
@@ -21,14 +21,14 @@ telefonski_broj varchar(50)
 );
 
 create table walleti(
-wallet_id int not null primary key identity(1,1)
+wallet_id int not null primary key identity(1,1),
 mreza varchar(50),
 korisnik_id int not null references korisnici(sifra),
 kljuc varchar(50) not null
 );
 
 create table kriptovalute(
-kripto_id int not null pirimary key(1,1),
+kripto_id int not null primary key identity(1,1),
 ime varchar(50),
 simbol varchar(50),
 cijena decimal(10,2),
@@ -36,15 +36,16 @@ trzisna_vrjednost decimal (15,2),
 volumen decimal(15,2)
 );
 
-crate table kriptowallet(
+create table kriptowallet(
 wallet_id int not null references walleti(wallet_id),
-kripto_id int not null references kriptovalute(wallet_id)
+kripto_id int not null references kriptovalute(kripto_id)
 );
 
 
 create table transakcije( 
-sifra int primary key identity(1,1)
+sifra int not null primary key identity(1,1),
  kolicina decimal(15,2),
- kripto_id int not not null,
- naknada decimal(10,2);
+ kripto_id int not null references kriptovalute(kripto_id),
+ naknada decimal(10,2)
+ );
 

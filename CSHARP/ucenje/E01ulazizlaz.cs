7517c6a9d026bba964ea4c09147d5bf76eba92e9
Gudelj22 <gudelj.petar2005@gmail.com>
2024-12-02 18:51:51 +0100

    ulaz izlaz

---
+++ CSHARP/ucenje/E01ulazizlaz.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ucenje
+{
+    internal class E01ulazizlaz
+    {
+
+
+        public static void izvedi() 
+        {
+            
+            
+            
+            //ovo je izlaz
+            Console.WriteLine("hello");
+            
+            
+            //ulaz podataka u program 
+            string Ime = Console.ReadLine();    
+            Console.WriteLine(Ime);
+
+        }
+       
+
+
+
+       
+        }
+}
--- CSHARP/ucenje/Program.cs
+++ CSHARP/ucenje/Program.cs
@@ -1,9 +1,13 @@
-﻿// ovo je komentar jedna linija
+﻿using ucenje;
+
+E01ulazizlaz.izvedi();
+
+// ovo je komentar jedna linija
 /*
  komentar vise linija
 
 */
 
 // See https://aka.ms/new-console-template for more information
-Console.WriteLine("Pozdrav Svijetu ćčđšž");
-Console.WriteLine("hello osijek");
+//Console.WriteLine("Pozdrav Svijetu ćčđšž");
+//Console.WriteLine("hello osijek");

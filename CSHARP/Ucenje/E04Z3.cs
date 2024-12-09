75a19531af1ce3706edc048cfce724c864978595
pinti88 <tin.pinta@gmail.com>
2024-12-08 21:12:21 -0800

    za kući

---
+++ CSHARP/Ucenje/E04Z3.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ucenje
+{
+
+    // Program od korisnika traži unos dva cijela broja
+    // Program ispisuje manji
+
+    // isti zadatak ali za tri unesena broja ispisati najmanji
+
+    internal class E04Z3
+    {
+        public static void Izvedi()
+        {
+            // Console.WriteLine("e04");
+
+            Console.WriteLine("Unesite prvi cijeli broj: ");
+            int prvibroj = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Unesite drugi cijeli broj: ");
+            int drugibroj = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Unesite treći cijeli broj: ");
+            int trecibroj = int.Parse(Console.ReadLine());
+
+            if (prvibroj < drugibroj && prvibroj < trecibroj)
+            {
+                Console.WriteLine("Manji broj je:" + prvibroj);
+            }
+            else if (drugibroj < prvibroj && drugibroj < trecibroj) 
+            {
+                Console.WriteLine("Manji broj je: " + drugibroj);
+            }
+            else 
+            {
+                Console.WriteLine("Manji broj je : " + trecibroj);
+            
+            
+            }
+
+
+
+
+            
+
+        }
+
+    }
+}
---
+++ CSHARP/Ucenje/E04Z4.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ucenje 
+{
+    /*
+            *  KORISTEĆI IF
+            * 
+           Program učitava od korisnika ime grada.U ovisnosti o imenu grada ispisuje regiju prema sljedećim pravilima:
+
+          Osijek -> Slavonija
+          Zadar -> Dalmacija
+          Čakovec -> Međimurje
+          Pula -> Istra
+          Za ostale unose program ispisuje: Ne znam koja je to regija.
+           */
+
+    // dodatno: Odraditi istu logiku s switch
+    internal class E04Z4
+    {
+        public static void Izvedi()
+        {
+
+            Console.WriteLine("Unesi naziv grada: ");
+            string grad = Console.ReadLine();
+            switch (grad)
+            {
+                case "Osijek": Console.WriteLine("Slavonija");
+                    break;
+                case "Zadar": Console.WriteLine("Dalmacija");
+                    break;
+                case "Čakovec": Console.WriteLine("Međimurje");
+                    break;
+                case "Pula": Console.WriteLine("Istra");
+                    break;
+                    default: Console.WriteLine("Ne znam koja je to regija.");
+                    break;
+                
+            }
+            
+        }
+
+        
+    }
+
+}
+
+
+    

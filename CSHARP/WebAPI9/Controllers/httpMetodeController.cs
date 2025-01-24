dfb5b3bd576d82e62b205ed2ebbc8fdc4c71f8d7
pinti88 <petargudelj96@gmail.com>
2025-01-24 18:53:07 +0100

    http Metode

---
+++ CSHARP/WebAPI9/Controllers/httpMetodeController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAPI9.Models;
+
+namespace WebAPI9.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class httpMetodeController : ControllerBase
+    {
+
+
+        // pocinje ruta 
+
+        // ruta je motoda 
+        [HttpGet]
+        
+        public string HelloWorld()
+        {
+            return "HelloWorld";
+        }
+
+        // zavrsava ruta
+
+        // pocinje ruta 
+
+        [HttpGet]
+        [Route("HelloWolrd")]
+        public string HelloWorld(string ime)
+        {
+            return $"Hello {ime}!";
+        }
+        //zavrsava ruta 
+
+
+        //pocinje ruta
+
+        [HttpGet]
+        [Route("json")]
+        public IActionResult Json(int sifra, string ime)
+        {
+            return Ok(new { Sifra = sifra, Ime = ime });
+        }
+
+        //zavrsava ruta
+
+
+
+        //pocinje ruta 
+
+        [HttpPost]
+        public IActionResult Post(Osoba osoba)
+        {
+            osoba.Ime = "hello" + osoba.Ime;
+            return StatusCode( 201, osoba);
+        }
+        //zavrsava ruta
+
+
+        //pocinje ruta
+
+
+        [HttpPut]
+        public IActionResult Put(Osoba osoba)
+        {
+            osoba.Ime = "Hello " + osoba.Ime;
+            return StatusCode(StatusCodes.Status206PartialContent);
+        }
+        // zavrsava ruta 
+
+
+        [HttpDelete]
+        public IActionResult Delete(int sifra)
+        {
+            if(sifra <= 0)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest, new { poruka = "Sifra mora biti veca o 0" });
+
+            }
+            return StatusCode(StatusCodes.Status400BadRequest);
+        }
+
+    }
+
+
+}
---
+++ CSHARP/WebAPI9/Models/Osoba.cs
@@ -0,0 +1,15 @@
+﻿namespace WebAPI9.Models
+{
+    public class Osoba
+    {
+
+        public int Sifra { get; set; }
+        public string? Ime { get; set; }
+        public decimal Koeficijent { get; set; }
+        public DateTime? DatimRodjenja { get; set; }
+        public bool Aktivan { get; set; }
+
+
+
+    }
+}

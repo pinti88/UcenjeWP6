4b9bdb74931e1a1a79ac597907cbd2cbcef28b3d
pinti88 <petargudelj96@gmail.com>
2025-01-27 18:00:56 +0100

    Priprema za smjer read

---
+++ CSHARP/EdunovaAPP/Models/Smjer.cs
@@ -0,0 +1,11 @@
+﻿namespace EdunovaAPP.Models
+{
+    public class Smjer : Entitet
+    {
+        public string Naziv { get; set; } = "";
+        public decimal Cijena { get; set; }
+        public DateTime? IzdoviSeOd { get; set; }
+        public bool Vaucer { get; set; }
+
+    }
+}

cd297595d016610e81ffc6bd7c11f2221eea1b5c
pinti88 <petargudelj96@gmail.com>
2025-01-31 17:45:46 +0100

    post put get delete

--- CSHARP/EdunovaAPP/Controllers/SmjerController.cs
+++ CSHARP/EdunovaAPP/Controllers/SmjerController.cs
@@ -1,4 +1,5 @@
 ﻿using EdunovaAPP.Data;
+using EdunovaAPP.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EdunovaAPP.Controllers
@@ -28,13 +29,110 @@ namespace EdunovaAPP.Controllers
                 return Ok(_context.Smjerovi);
 
             }
-            catch(Exception e)
+            catch (Exception e)
             {
                 return BadRequest(e);
             }
         }
 
 
+        [HttpGet]
+        [Route("{sifra:int}")]
+        public IActionResult GetBySifra(int sifra)
+        {
+            try
+            {
+                var s = _context.Smjerovi.Find(sifra);
+                if (s == null)
+                {
+                    return NotFound();
+                }
+                return Ok(s);
+
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Post(Smjer smjer)
+        {
+            try
+            {
+                _context.Smjerovi.Add(smjer);
+                _context.SaveChanges();
+                return StatusCode(StatusCodes.Status201Created, smjer);
+
+
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e);
+            }
+
+
+
+        }
+
+
+        [HttpPut]
+        [Route("{sifra:int}")]
+        [Produces("Application/json")]
+        public IActionResult Put(int sifra, Smjer smjer)
+        {
+            try
+            {
+                var s = _context.Smjerovi.Find(sifra);
+                if (s == null)
+                {
+                    return NotFound();
+                }
+                s.Naziv = smjer.Naziv;
+                s.CijenaSmjera = smjer.CijenaSmjera;
+                s.IzvodiSeOd = smjer.IzvodiSeOd;
+                s.Vaucer = smjer.Vaucer;
+
+                _context.Smjerovi.Update(s);
+                _context.SaveChanges();
+                return Ok(new { poruka = "uspjesno promjenjeno" });
+
+
+
+
+            }
+            catch (Exception e)
+
+            {
+                return BadRequest(e);
+            }
+
+
+        }
+
+        [HttpDelete]
+        [Route("{sifra:int}")]
+        public IActionResult Delete(int sifra)
+        {
+            try
+            {
+                var s = _context.Smjerovi.Find(sifra);
+                if(s== null)
+                {
+                    return NotFound();
+                }
+                _context.Smjerovi.Remove(s);
+                _context.SaveChanges();
+                return Ok(new { poruka = "uspjesno dodana" });
+
+            }
+            catch(Exception e)
+            {
+                return BadRequest();
+            }
+
+        }
 
 
     }
--- CSHARP/EdunovaAPP/Models/Smjer.cs
+++ CSHARP/EdunovaAPP/Models/Smjer.cs
@@ -6,7 +6,7 @@ namespace EdunovaAPP.Models
     {
         public string Naziv { get; set; } = "";
         [Column("cijena")] // ovo je bit EF, mogucnost mapiranja
-        public decimal? Cijena { get; set; }
+        public decimal? CijenaSmjera { get; set; }
         public DateTime? IzvodiSeOd { get; set; }
         public bool? Vaucer { get; set; }
 
